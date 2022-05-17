84da6f8906263d973a2198706f8ee0ea9c0109bb
User <user@Userui-MacBook-Pro.local>
2022-05-17 23:37:56 +0900

    Initial commit

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();//동시성이슈때문에 Conquered Hashmap을 써야하는데
+    //여기서는 시간상 일반 Hashmap
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
